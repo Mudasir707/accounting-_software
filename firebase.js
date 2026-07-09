24a2d707ac894a7738a24c10d778816293f1b937
Mudasir707 <shymalik707@gmail.com>
2026-07-10 05:27:33 +0530

    Initialize Firebase app with configuration

--- firebase.js
+++ firebase.js
@@ -1,1 +1,16 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOQx6wMeSQ4IJQdg0Ex1LZOfJaNgdV28Q",
+  authDomain: "sheen-accounts.firebaseapp.com",
+  projectId: "sheen-accounts",
+  storageBucket: "sheen-accounts.firebasestorage.app",
+  messagingSenderId: "674397953304",
+  appId: "1:674397953304:web:cbf1292d1d56091101126d",
+  measurementId: "G-1PQS5V7NLY"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected Successfully!");
