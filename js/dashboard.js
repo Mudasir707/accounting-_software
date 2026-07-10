f90bd0b5b8dc51f0d6d916d523383af2771750c5
Mudasir707 <shymalik707@gmail.com>
2026-07-10 06:30:08 +0530

    Load customer count from Firestore in dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,17 @@
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+import { app } from "./firebase.js";
+
+const db = getFirestore(app);
+
+async function loadDashboard() {
+    const customers = await getDocs(collection(db, "customers"));
+
+    document.getElementById("customerCount").innerText = customers.size;
+}
+
+loadDashboard();
