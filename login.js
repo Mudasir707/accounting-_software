019bdfbdd7d22a4760bead63d2f5a957e71883ce
Mudasir707 <shymalik707@gmail.com>
2026-07-18 17:56:37 +0530

    Implement basic login functionality

--- login.js
+++ login.js
@@ -1,1 +1,21 @@
+const USERNAME = "admin";
+const PASSWORD = "123456";
 
+document.getElementById("loginBtn").addEventListener("click", function () {
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value;
+
+    if (username === USERNAME && password === PASSWORD) {
+
+        alert("Login Successful");
+
+        window.location.href = "dashboard.html";
+
+    } else {
+
+        alert("Invalid Username or Password");
+
+    }
+
+});
