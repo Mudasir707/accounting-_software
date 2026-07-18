92f14f22512fca5578d92b8a1c3a04ea20a70e67
Mudasir707 <shymalik707@gmail.com>
2026-07-18 17:01:18 +0530

    Implement calculateTotal and saveSale functions
    Added functions to calculate total and save sale details.

--- sales.js
+++ sales.js
@@ -1,1 +1,23 @@
+function calculateTotal() {
+    const qty = Number(document.getElementById("qty").value) || 0;
+    const rate = Number(document.getElementById("rate").value) || 0;
 
+    document.getElementById("total").value = qty * rate;
+}
+
+function saveSale() {
+
+    const sale = {
+        invoiceNo: document.getElementById("invoiceNo").value,
+        date: document.getElementById("invoiceDate").value,
+        customer: document.getElementById("customerName").value,
+        item: document.getElementById("itemName").value,
+        qty: Number(document.getElementById("qty").value),
+        rate: Number(document.getElementById("rate").value),
+        total: Number(document.getElementById("total").value)
+    };
+
+    console.log("Sale Saved", sale);
+
+    alert("Invoice Saved Successfully!");
+}
